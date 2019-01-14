c4497fb82287ae4834a37b94a10b64eb02eda997
Arcadi Garcia <arcadigarciarius21@gmail.com>
2019-01-14 21:55:03 +0100

    Update getMatchNames.jsx

--- getMatchNames.jsx
+++ getMatchNames.jsx
@@ -2,19 +2,18 @@
   function test(){
     var fx, s, n, key;
 
-    if (app.project && app.project.activeItem instanceof CompItem && app.project.activeItem.selectedProperties.length>0 && (fx = app.project.activeItem.selectedProperties[0]).isEffect) {
-        s = "for... in...\r";
-        for (key in fx) s += "\r" + key;
-        alert (s);
+    if (app.project && app.project.activeItem instanceof CompItem && app.project.activeItem.selectedProperties.length>0) {
+        fx = app.project.activeItem.selectedProperties[0];
+        s = fx.name + " belongs to:\r";
 
-        s = "for...in... Own js properties: \r";     // >>> apparently, always empty
-        for (key in fx && fx.hasOwnProperty(key)) s += "\r" + key;
-        alert (s);
-
-        s = "AE properties: \r";
+        for(n=1; n<=fx.propertyDepth; n++){
+          s += fx.propertyGroup(n).name + " < ";
+        }
+        s += "\rAE properties of: " + fx.name + "\r";
         for (n =1; n<= fx.numProperties; n++)
           s += "\r" + fx.property(n).matchName;
-        alert (s);
+
+        alert(s);
       }
     }
     test();
